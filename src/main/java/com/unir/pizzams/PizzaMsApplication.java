5d6d2eb851473d6a42b05ce9d9d3742210888d83
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2023-06-24 23:53:51 +0200

    DOCENCIA-37 Beta Private Networking railway

--- src/main/java/com/unir/pizzams/PizzaMsApplication.java
+++ src/main/java/com/unir/pizzams/PizzaMsApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PizzaMsApplication {
 
 	public static void main(String[] args) {
+
+		// Obtenemos perfil de ejecucion de variable de entorno. Si no hay, perfil default.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
 		SpringApplication.run(PizzaMsApplication.class, args);
 	}
 
