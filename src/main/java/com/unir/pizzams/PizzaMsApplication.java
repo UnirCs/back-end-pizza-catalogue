15ea85581a181b91be4c4dd2ee51680f71786843
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2023-06-25 20:03:56 +0200

    SLEEP because of internal routing startup

--- src/main/java/com/unir/pizzams/PizzaMsApplication.java
+++ src/main/java/com/unir/pizzams/PizzaMsApplication.java
@@ -3,7 +3,10 @@ package com.unir.pizzams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class PizzaMsApplication {
 
 	public static void main(String[] args) {
@@ -11,6 +14,14 @@ public class PizzaMsApplication {
 		// Obtenemos perfil de ejecucion de variable de entorno. Si no hay, perfil default.
 		String profile = System.getenv("PROFILE");
 		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		
+		log.debug("Waiting fo Internal Interface to start...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		SpringApplication.run(PizzaMsApplication.class, args);
 	}
 
